c0b27750646dbc9c6c2fabe782cfd999ae551fb5
Martynas Simanavicius <martynas.simanavicius2@gmail.com>
2025-07-20 20:36:27 +0300

    update initial files, to use migrations logic

--- app/__init__.py
+++ app/__init__.py
@@ -2,6 +2,7 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from .config import Config
+from .models import *
 
 db = SQLAlchemy()
 migrate = Migrate()
---
+++ app/routes.py
@@ -0,0 +1,11 @@
+from flask import Blueprint, jsonify
+
+main = Blueprint('main', __name__)
+
+@main.route('/')
+def home():
+    return jsonify({"message": "Welcome to the Flask API!"})
+
+@main.route('/ping')
+def ping():
+    return jsonify({"message": "pong"})
