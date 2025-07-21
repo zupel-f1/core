5105e014bbe913d867e2140acb586bf58e8181f8
Martynas Simanavicius <martynas.simanavicius2@gmail.com>
2025-07-21 18:14:38 +0300

    fix naming mistake for race_driver_result.py

--- app/models/__init__.py
+++ app/models/__init__.py
@@ -2,5 +2,5 @@ from .season import Season
 from .driver import Driver
 from .circuit import Circuit
 from .constructor import Constructor
-from .race_driver_resuilt import RaceDriverResult
+from .race_driver_result import RaceDriverResult
 from .races import Race
