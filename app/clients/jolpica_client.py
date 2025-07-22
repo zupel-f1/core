e5951c113e9b8d15cb983f79b415555ee3bd9d20
R. Valuckas <rokas.valuckas@gmail.com>
2025-07-22 22:59:30 +0300

    HALO-32 add jolpica client
    Add requests to requirements.txt
    Created jolpica client for GET requests
    
    Ticket: https://zupel.atlassian.net/browse/HALO-32

---
+++ app/clients/jolpica_client.py
@@ -0,0 +1,9 @@
+import requests
+def fetch_from_jolpica(endpoint: str, params: dict | None = None):
+    base_url = 'https://api.jolpi.ca/ergast/f1'
+    endpoint = f'{base_url}/{endpoint}'
+    initial_response = requests.get(endpoint, params=params)
+    initial_response.raise_for_status()
+    response = initial_response.json()
+    return response
+
