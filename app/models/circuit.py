92b9818730bab28e84c403342e2757499123aecc
R. Valuckas <rokas.valuckas@gmail.com>
2025-08-02 17:03:03 +0300

    Add migration with new models

--- app/models/circuit.py
+++ app/models/circuit.py
@@ -5,7 +5,8 @@ class Circuit(db.Model):
     __tablename__ = 'circuits'
     
     id = db.Column(db.Integer, primary_key=True)
+    circuit_id = db.Column(db.String(255), nullable=False)
     url = db.Column(db.String(255), nullable=False)
     circuit_name = db.Column(db.String(255), nullable=False)
     city = db.Column(db.String(255), nullable=False)
-    country_emoji = db.Column(db.String(100), nullable=False)
+    country = db.Column(db.String(100), nullable=False)
--- app/models/races.py
+++ app/models/races.py
@@ -8,6 +8,7 @@ class Race(db.Model):
     season_id = db.Column(db.Integer, db.ForeignKey('seasons.id'), nullable=False)
     circuit_id = db.Column(db.Integer, db.ForeignKey('circuits.id'), nullable=False)
     race_name = db.Column(db.String(255), nullable=False)
+    is_sprint = db.Column(db.Boolean, nullable=False)
     date = db.Column(db.Date, nullable=False)
     url = db.Column(db.String(255), nullable=False)
     round = db.Column(db.Integer, nullable=False)
