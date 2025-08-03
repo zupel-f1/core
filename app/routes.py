186d31a311b358631447d4d17f38d1e5f909b2e0
Martynas Simanavicius <martynas.simanavicius2@gmail.com>
2025-08-03 19:47:05 +0300

    remove unused routes path for fetching drivers

--- app/routes.py
+++ app/routes.py
@@ -9,12 +9,3 @@ def home():
 @main.route('/ping')
 def ping():
     return jsonify({"message": "pong"})
-
-@main.route('/drivers')
-def get_drivers():
-    from app.services.fetch_drivers import run as fetch_drivers
-    try:
-        fetch_drivers()
-        return jsonify({"message": "Drivers fetched successfully!"})
-    except Exception as e:
-        return jsonify({"error": str(e)}), 500
