d295d8d09b0460b8c1d65dc310d01226aff51b5f
Jokūbas <69209888+Jokubas2@users.noreply.github.com>
2025-08-03 11:59:48 +0300

    Constructor fetch

---
+++ app/services/fetch_constructors.py
@@ -0,0 +1,60 @@
+from app.clients.jolpica_client import fetch_from_jolpica
+from app.services import MAX_LIMIT
+from app.models.constructor import Constructor
+from app import create_app
+from app.extensions import db
+
+
+def run():
+    app = create_app()
+    with app.app_context():
+        constructors = fetch_constructors_from_jolpica()
+        constructors = transform_constructors_data(constructors)
+        update_database_constructors(constructors)
+
+
+def fetch_constructors_from_jolpica():
+    all_constructors = []
+    offset = 0
+    limit = MAX_LIMIT
+    while True:
+        constructors_data = fetch_from_jolpica("constructors", {"limit": limit, "offset": offset})
+        returned_constructors = constructors_data["MRData"]["ConstructorTable"]["Constructors"]
+        if not returned_constructors:
+            break
+        all_constructors.extend(returned_constructors)
+        if len(returned_constructors) < limit:
+            break
+        offset += limit
+    return all_constructors
+
+
+def transform_constructors_data(constructors):
+    transformed = []
+    for constructor in constructors:
+        transformed.append({
+            "external_id": constructor["constructorId"],
+            "url": constructor["url"],
+            "name": constructor["name"],
+            "nationality": constructor["nationality"],
+        })
+    return transformed
+
+
+def update_database_constructors(constructors):
+    for constructor in constructors:
+        existing = db.session.query(Constructor).filter(Constructor.external_id == constructor["external_id"]).count()
+        if existing > 0:
+            continue
+        constructor = Constructor(
+            external_id=constructor["external_id"],
+            name=constructor["name"],
+            url=constructor["url"],
+            nationality=constructor["nationality"],
+        )
+        db.session.add(constructor)
+    db.session.commit()
+
+
+if __name__ == "__main__":
+    run()
