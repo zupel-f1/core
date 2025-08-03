80cab83d5b9c5f746ffdd83478a10c41ddd18122
Martynas Simanavicius <martynas.simanavicius2@gmail.com>
2025-08-03 20:09:16 +0300

    indentation fixes, some optimizations

--- app/services/fetch_drivers.py
+++ app/services/fetch_drivers.py
@@ -2,14 +2,15 @@ from app import create_app
 from app.clients.jolpica_client import fetch_from_jolpica
 from app.extensions import db
 from app.models.driver import Driver
+from app.services import MAX_LIMIT
 
 def run():
     drivers = _fetch_drivers_from_jolpica()
     drivers = _transform_drivers_data(drivers)
     _update_database_drivers(drivers)
 
+
 def _fetch_drivers_from_jolpica():
-    MAX_LIMIT = 100
     all_drivers = []
     offset = 0
     limit = MAX_LIMIT
@@ -23,6 +24,7 @@ def _fetch_drivers_from_jolpica():
             break
         offset += limit
     return all_drivers
+
 
 def _transform_drivers_data(drivers):
     transformed = []
@@ -42,16 +44,16 @@ def _transform_drivers_data(drivers):
 
 
 def _update_database_drivers(drivers):
-     for driver_data in drivers:
-        driver = Driver.query.filter_by(external_id=driver_data["external_id"]).first()
+    for driver_data in drivers:
+        driver = Driver.query.filter_by(external_id=driver_data["external_id"]).one_or_none()
         if driver:
             for key, value in driver_data.items():
                 setattr(driver, key, value)
         else:
             driver = Driver(**driver_data)
             db.session.add(driver)
+    db.session.commit()
 
-        db.session.commit()
 
 if __name__ == "__main__":
     app = create_app()
