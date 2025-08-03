21f8859583294df92f475282753a3dbaccb49b82
Jokūbas <69209888+Jokubas2@users.noreply.github.com>
2025-08-03 20:11:23 +0300

    Added constructor tests (inspired by martynas)

---
+++ tests/services/test_fetch_constructors.py
@@ -0,0 +1,63 @@
+import pytest
+from unittest.mock import patch
+from app.services import fetch_constructors
+
+
+@pytest.fixture
+def sample_constructor_data():
+    return [
+        {
+            "constructorId": "alpine",
+            "url": "http://en.wikipedia.org/wiki/Alpine_F1_Team",
+            "name": "Alpine F1 Team",
+            "nationality": "French"
+        }
+    ]
+
+
+@patch("app.services.fetch_constructors.fetch_from_jolpica")
+def test_fetch_constructors_from_jolpica(mock_fetch):
+    mock_fetch.side_effect = [
+        {
+            "MRData": {
+                "ConstructorTable": {
+                    "Constructors": [
+                        {"constructorId": "alpine",
+                         "url": "http://en.wikipedia.org/wiki/Alpine_F1_Team",
+                         "name": "Alpine F1 Team",
+                         "nationality": "French"}
+                    ]
+                }
+            }
+        },
+        {
+            "MRData": {
+                "ConstructorTable": {
+                    "Constructors": []
+                }
+            }
+        }
+    ]
+    constructors = fetch_constructors._fetch_constructors_from_jolpica()
+    assert len(constructors) == 1
+    assert constructors[0]["constructor_id"] == "alpine"
+
+
+def test_transform_constructors_data(sample_driver_data):
+    transformed = fetch_constructors._transform_constructors_data(sample_driver_data)
+    assert transformed == [{
+        "external_id": "alpine",
+        "url": "http://en.wikipedia.org/wiki/Alpine_F1_Team",
+        "name": "Alpine F1 Team",
+        "nationality": "French"
+    }]
+
+
+@patch("app.services.fetch_constructors.Constructor")
+@patch("app.services.fetch_constructors.db")
+def test_update_database_drivers(mock_db, mock_constructor, sample_constructor_data):
+    mock_constructor.query.filter_by.return_value.first.return_value = None
+    constructors = fetch_constructors._transform_constructors_data(sample_constructor_data)
+    fetch_constructors._update_database_constructors(constructors)
+    assert mock_db.session.add.call_count == 1
+    assert mock_db.session.commit.call_count == 1
