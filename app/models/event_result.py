6eb3a103ab1dd99489de0d53d80c3bafe6647a6c
Jokūbas <69209888+Jokubas2@users.noreply.github.com>
2025-08-17 14:48:24 +0300

    Unique constraint for event_result

--- app/models/event_result.py
+++ app/models/event_result.py
@@ -1,13 +1,22 @@
+from sqlalchemy import UniqueConstraint
+
 from app.extensions import db
 
 
 class EventResult(db.Model):
-    __tablename__ = 'event_results'
+    __tablename__ = "event_results"
+    __table_args__ = (
+        UniqueConstraint(
+            "event_id", "driver_id", name="idx_event_results_on_event_id_and_driver_id"
+        ),
+    )
 
     id = db.Column(db.Integer, primary_key=True)
-    event_id = db.Column(db.Integer, db.ForeignKey('events.id'), nullable=False)
-    driver_id = db.Column(db.Integer, db.ForeignKey('drivers.id'), nullable=False)
-    constructor_id = db.Column(db.Integer, db.ForeignKey('constructors.id'), nullable=True)
+    event_id = db.Column(db.Integer, db.ForeignKey("events.id"), nullable=False)
+    driver_id = db.Column(db.Integer, db.ForeignKey("drivers.id"), nullable=False)
+    constructor_id = db.Column(
+        db.Integer, db.ForeignKey("constructors.id"), nullable=True
+    )
     position = db.Column(db.Integer, nullable=False)
     time = db.Column(db.Integer, nullable=False)
     points = db.Column(db.Float, nullable=False)
