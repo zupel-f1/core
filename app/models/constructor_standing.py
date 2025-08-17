b3707acb599c49bdf5a524bf30a8315225103c7f
Jokūbas <69209888+Jokubas2@users.noreply.github.com>
2025-08-17 15:30:11 +0300

    Added constructor and driver standings

---
+++ app/models/constructor_standing.py
@@ -0,0 +1,18 @@
+from sqlalchemy import UniqueConstraint
+
+from app.extensions import db
+
+
+class ConstructorStanding(db.Model):
+    __tablename__ = "constructor_standings"
+    __table_args__ = (
+        UniqueConstraint(
+            "constructor_id", "round_id", name="idx_constructor_standings_on_constructor_id_and_round_id"
+        ),
+    )
+
+    id = db.Column(db.Integer, primary_key=True)
+    constructor_id = db.Column(db.Integer, db.ForeignKey("constructors.id"), nullable=False)
+    round_id = db.Column(db.Integer, db.ForeignKey("rounds.id"), nullable=False)
+    points = db.Column(db.Float, nullable=False)
+    position = db.Column(db.Integer, nullable=False)
---
+++ app/models/driver_standing.py
@@ -0,0 +1,18 @@
+from sqlalchemy import UniqueConstraint
+
+from app.extensions import db
+
+
+class DriverStanding(db.Model):
+    __tablename__ = "driver_standings"
+    __table_args__ = (
+        UniqueConstraint(
+            "driver_id", "round_id", name="idx_driver_standings_on_driver_id_and_round_id"
+        ),
+    )
+
+    id = db.Column(db.Integer, primary_key=True)
+    driver_id = db.Column(db.Integer, db.ForeignKey("drivers.id"), nullable=False)
+    round_id = db.Column(db.Integer, db.ForeignKey("rounds.id"), nullable=False)
+    points = db.Column(db.Float, nullable=False)
+    position = db.Column(db.Integer, nullable=False)
