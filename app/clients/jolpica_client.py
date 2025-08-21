d22f17ecd52c996c165e94017f62c1f7f9158bba
Jokūbas <69209888+Jokubas2@users.noreply.github.com>
2025-08-21 23:16:41 +0300

    Fetch rounds

--- app/clients/jolpica_client.py
+++ app/clients/jolpica_client.py
@@ -1,7 +1,10 @@
+import time
+
 import requests
 
 
 def fetch_from_jolpica(endpoint: str, params: dict | None = None):
+    time.sleep(7.5)
     base_url = "https://api.jolpi.ca/ergast/f1"
     endpoint = f"{base_url}/{endpoint}"
     initial_response = requests.get(endpoint, params=params)
